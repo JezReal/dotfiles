00c095b0d2f8d927f3a90bcf77e71c97fa2c1bee
Jezreel <jezreeljoshuam@gmail.com>
2024-07-13 16:56:28 +0800

    update gitsigns

--- .config/nvim/lua/custom/plugins/gitsigns.lua
+++ .config/nvim/lua/custom/plugins/gitsigns.lua
@@ -1,8 +1,24 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
-        require('gitsigns').setup({
-            current_line_blame = true
-        })
-    end,
+		require("gitsigns").setup({
+			current_line_blame = true,
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Actions
+				map("n", "<leader>hp", gitsigns.preview_hunk)
+				map("n", "<leader>hb", function()
+					gitsigns.blame_line({ full = true })
+				end)
+				map("n", "<leader>hd", gitsigns.diffthis)
+			end,
+		})
+	end,
 }
