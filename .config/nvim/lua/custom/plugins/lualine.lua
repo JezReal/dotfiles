ee0477cfefdab75b77ee16eda80143d6e8496e37
Jezreel <jezreeljoshuam@gmail.com>
2024-07-13 16:42:12 +0800

    update config

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -1,19 +1,24 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = "tokyonight",
-            },
-            sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {},
-                lualine_x = {},
-                lualine_y = { "branch", "diff" },
-                lualine_z = { "encoding", "fileformat", "filetype" },
-            },
-        })
-    end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "tokyonight",
+			},
+			sections = {
+				lualine_a = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = { "branch", "diff" },
+				lualine_z = { "encoding", "fileformat", "filetype" },
+			},
+		})
+	end,
 }
