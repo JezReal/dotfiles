7cc2bee53ed917d79dc0fff66eae8e9a7dc9bded
jez <jezreeljoshuam@gmail.com>
2024-10-06 17:51:24 +0800

    completion stuff

--- .config/nvim/lua/custom/plugins/completion.lua
+++ .config/nvim/lua/custom/plugins/completion.lua
@@ -1,50 +1,88 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
+    {
+        "hrsh7th/cmp-nvim-lsp",
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+            "hrsh7th/cmp-nvim-lsp-signature-help",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-path",
+            "f3fora/cmp-spell",
+        },
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require("cmp")
             local luasnip = require("luasnip")
 
-			require("luasnip.loaders.from_vscode").lazy_load()
-            luasnip.filetype_extend("templ", {"html"})
-            luasnip.filetype_extend("htmldjango", {"html"})
-            luasnip.filetype_extend("vue", {"html"})
-            luasnip.filetype_extend("svelte", {"html"})
+            require("luasnip.loaders.from_vscode").lazy_load()
+            luasnip.filetype_extend("templ", { "html" })
+            luasnip.filetype_extend("htmldjango", { "html" })
+            luasnip.filetype_extend("vue", { "html" })
+            luasnip.filetype_extend("svelte", { "html" })
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "path" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "nvim_lsp_signature_help" },
+                    {
+                        name = "spell",
+                        option = {
+                            keep_all_entries = false,
+                            enable_in_context = function()
+                                return true
+                            end,
+                            preselect_correct_word = true,
+                        },
+                    },
+                },
+            })
+
+            cmp.setup.cmdline("/", {
+                mapping = cmp.mapping.preset.cmdline(),
+
+                sources = {
+                    { name = "buffer" },
+                },
+            })
+
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                }, {
+                    {
+                        name = "cmdline",
+                        option = {
+                            ignore_cmds = { "Man", "!" },
+                        },
+                    },
+                }),
+            })
+        end,
+    },
 }
--- .config/nvim/lua/custom/set.lua
+++ .config/nvim/lua/custom/set.lua
@@ -30,3 +30,6 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "100"
+
+vim.opt.spell = true
+vim.opt.spelllang = { "en_us" }
