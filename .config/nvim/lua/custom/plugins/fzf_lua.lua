2050a3e27582b291bad50b5842e21db7766f4684
jez <jezreeljoshuam@gmail.com>
2024-10-13 22:05:54 +0800

    update trouble and integrate with fzf lua

--- .config/nvim/lua/custom/plugins/fzf_lua.lua
+++ .config/nvim/lua/custom/plugins/fzf_lua.lua
@@ -1,29 +1,34 @@
 return {
-    {
-        "ibhagwan/fzf-lua",
-        config = function()
-            require("fzf-lua").setup({
-                require("fzf-lua").register_ui_select(),
-                defaults = {
-                    file_icons = false
-                }
-            })
+	{
+		"ibhagwan/fzf-lua",
+		config = function()
+			local config = require("fzf-lua.config")
 
-            vim.keymap.set("n", "<leader>fz", function()
-                vim.cmd("FzfLua builtin")
-            end)
-            vim.keymap.set("n", "<leader>af", function()
-                vim.cmd("FzfLua files")
-            end)
-            vim.keymap.set("n", "<leader>gf", function()
-                vim.cmd("FzfLua git_files")
-            end)
-            vim.keymap.set("n", "<leader>lg", function()
-                vim.cmd("FzfLua live_grep")
-            end)
-            vim.keymap.set("n", "<leader>fg", function()
-                vim.cmd("FzfLua lgrep_curbuf")
-            end)
-        end
-    }
+			local actions = require("trouble.sources.fzf").actions
+			config.defaults.actions.files["ctrl-t"] = actions.open
+
+			require("fzf-lua").setup({
+				require("fzf-lua").register_ui_select(),
+				defaults = {
+					file_icons = false,
+				},
+			})
+
+			vim.keymap.set("n", "<leader>fz", function()
+				vim.cmd("FzfLua builtin")
+			end)
+			vim.keymap.set("n", "<leader>af", function()
+				vim.cmd("FzfLua files")
+			end)
+			vim.keymap.set("n", "<leader>gf", function()
+				vim.cmd("FzfLua git_files")
+			end)
+			vim.keymap.set("n", "<leader>lg", function()
+				vim.cmd("FzfLua live_grep")
+			end)
+			vim.keymap.set("n", "<leader>fg", function()
+				vim.cmd("FzfLua lgrep_curbuf")
+			end)
+		end,
+	},
 }
--- .config/nvim/lua/custom/plugins/trouble.lua
+++ .config/nvim/lua/custom/plugins/trouble.lua
@@ -1,36 +1,14 @@
 return {
-	"folke/trouble.nvim",
-	opts = {
-		icons = false,
-		fold_open = "v",
-		fold_closed = ">",
-		indent_lines = false,
-		signs = {
-			error = "error",
-			warning = "warn",
-			hint = "hint",
-			information = "info",
-		},
-		use_diagnostic_signs = false,
-	},
-	config = function()
-		vim.keymap.set("n", "<leader>tr", function()
-			require("trouble").toggle()
-		end)
-		vim.keymap.set("n", "<leader>tw", function()
-			require("trouble").toggle("workspace_diagnostics")
-		end)
-		vim.keymap.set("n", "<leader>td", function()
-			require("trouble").toggle("document_diagnostics")
-		end)
-		vim.keymap.set("n", "<leader>tf", function()
-			require("trouble").toggle("quickfix")
-		end)
-		vim.keymap.set("n", "<leader>tl", function()
-			require("trouble").toggle("loclist")
-		end)
-		vim.keymap.set("n", "tlr", function()
-			require("trouble").toggle("lsp_references")
-		end)
-	end,
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    opts = {
+        icons = {},
+    },
+    keys = {
+        {
+            "<leader>td",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+    },
 }
