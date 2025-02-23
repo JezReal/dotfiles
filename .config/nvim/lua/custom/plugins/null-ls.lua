c9d24d8c3b0eb8cc9f16adcd239f43717d3eb574
jez <jezreeljoshuam@gmail.com>
2025-02-23 17:33:34 +0800

    formattign stuff

--- .config/nvim/lua/custom/plugins/null-ls.lua
+++ .config/nvim/lua/custom/plugins/null-ls.lua
@@ -10,7 +10,8 @@ return {
                 null_ls.builtins.formatting.eslint_d,
                 null_ls.builtins.diagnostics.golangci_lint,
                 null_ls.builtins.diagnostics.rubocop,
-                null_ls.builtins.formatting.rubocop
+                null_ls.builtins.formatting.rubocop,
+                null_ls.builtins.formatting.yamlfmt,
             },
         })
     end,
