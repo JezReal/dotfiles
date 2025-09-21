b1f8df08c96e276972af6cbe07d8791b74c31e69
jez <jezreeljoshuam@gmail.com>
2025-09-21 14:57:47 +0800

    update nulll ls

--- .config/nvim/lua/custom/plugins/null-ls.lua
+++ .config/nvim/lua/custom/plugins/null-ls.lua
@@ -1,16 +1,19 @@
 return {
     "nvimtools/none-ls.nvim",
-    commit = "2236d2bf621b64ccbceebc452137b25ecc7f6228",
+    -- commit = "2236d2bf621b64ccbceebc452137b25ecc7f6228",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
+
     config = function()
         local null_ls = require("null-ls")
 
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.eslint_d,
+                -- null_ls.builtins.formatting.eslint_d,
+                require("none-ls.formatting.eslint_d"),
                 null_ls.builtins.diagnostics.golangci_lint,
-                null_ls.builtins.diagnostics.rubocop,
-                null_ls.builtins.formatting.rubocop,
                 null_ls.builtins.formatting.yamlfmt,
             },
         })
