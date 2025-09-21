76eac61369ab02d36b8377e8a790dcf8ce73805d
jez <jezreeljoshuam@gmail.com>
2025-09-21 14:56:47 +0800

    lsp stuff

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -1,142 +1,37 @@
 return {
 	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"gopls",
+				"ts_ls",
+				"html",
+				"emmet_language_server",
+				"templ",
+				"svelte",
+				"tailwindcss",
+				"htmx",
+				"cssls",
+				"spectral",
+			},
+		},
 	},
 	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"gopls",
-					"ts_ls",
-					"html",
-					"emmet_language_server",
-					"templ",
-					"volar",
-					"svelte",
-					"astro",
-					"tailwindcss",
-					"htmx",
-					"cssls",
-					"spectral",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-				settings = {
-					gopls = {
-						analyses = {
-							unusedparams = true,
-						},
-						completeUnimported = true,
-						staticcheck = true,
-						gofumpt = true,
-					},
-				},
-				filetypes = { "go", "gomod", "gowork", "gotmpl", "templ" },
-			})
-
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-				init_options = {
-					preferences = {
-						disableSuggestions = true,
-					},
-				},
-			})
-
-			lspconfig.html.setup({
-				capabilities = capabilities,
-				filetypes = { "html", "templ", "htmldjango"},
-			})
-			lspconfig.templ.setup({
-				vim.filetype.add({ extension = { templ = "templ" } }),
-				capabilities = capabilities,
-			})
-
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.volar.setup({
-				capabilities = capabilities,
-				filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact" },
-				init_options = {
-					vue = {
-						hybridMode = false,
-					},
-					typescript = {
-						tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
-					},
-				},
-			})
-
-			lspconfig.svelte.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.astro.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.htmx.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.spectral.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.prismals.setup({
-				capabilities = capabilities,
-			})
-
-			-- keymaps
-			vim.keymap.set("n", "gd", function()
-				vim.lsp.buf.definition()
-			end, opts)
-			vim.keymap.set("n", "K", function()
-				vim.lsp.buf.hover()
-			end, opts)
-			vim.keymap.set("n", "<leader>ws", function()
-				vim.lsp.buf.workspace_symbol()
-			end, opts)
-			vim.keymap.set("n", "<leader>ca", function()
-				vim.lsp.buf.code_action()
-			end, opts)
-			vim.keymap.set("n", "<leader>cr", function()
-				vim.lsp.buf.references()
-			end, opts)
-			vim.keymap.set("n", "<leader>rn", function()
-				vim.lsp.buf.rename()
-			end, opts)
-			vim.keymap.set("i", "<C-h>", function()
-				vim.lsp.buf.signature_help()
-			end, opts)
-		end,
+		vim.diagnostic.config({
+			virtual_lines = true,
+			-- virtual_text = true,
+			underline = true,
+			update_in_insert = false,
+			severity_sort = true,
+			float = {
+				border = "rounded",
+				source = true,
+			},
+		}),
 	},
 }
